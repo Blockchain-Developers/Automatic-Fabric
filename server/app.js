2e617454e481af6246fea51af9558b23e3fe1461
root <root@ip-172-31-94-82.ec2.internal>
2020-04-29 02:45:07 +0000

    minor changes took care of bugs

--- server/app.js
+++ server/app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var getconfigRouter = require('./routes/getconfig');
+var createRouter = require('./routes/create');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/create', getconfigRouter);
+app.use('/create', createRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
