285cedbfc6b5af68ea17bfd820d2d83e03966a2d
root <root@ip-172-31-94-82.ec2.internal>
2020-04-29 03:26:06 +0000

    add module

---
+++ ui-dev/build/precache-manifest.52a2974fc019b8c9d9c94f7cdc6185c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b61548184080050fff5ce609e3df0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5911e5b11ac2790fc0",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8a7a508962aaedb1c5ab",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b5911e5b11ac2790fc0",
+    "url": "/static/js/2.8ae76165.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.8ae76165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7a508962aaedb1c5ab",
+    "url": "/static/js/main.f153d32e.chunk.js"
+  },
+  {
+    "revision": "0b48a607a9b57d0a9f6a",
+    "url": "/static/js/runtime-main.760a49e5.js"
+  }
+]);
