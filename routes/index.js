63ecb16da63d4e3f97c1e67f2ecf36310b1641da
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-04-29 11:21:07 +0800

    landing page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const fetch = require('node-fetch');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Automatic-Fabric' });
+router.get('/', async function(req, res, next) {
+  var user=await fetch('/user');
+  res.render('index', { user:user });
 });
 module.exports = router;
