dceca7f0760e7a875a23d59df9d1bae51a168131
root <root@ip-172-31-94-82.ec2.internal>
2020-04-29 06:55:57 +0000

    fixed session bug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'nodejs',
+  password: 'nodejspassword',
+  database: 'users',
+  multipleStatements: true
+});
+
+var sessionStore = new MySQLStore({}, con);
+
 /* GET home page. */
+router.use(session(
+{
+        secret: 'asdfasdI0YOYiAxjOtR',
+        resave: true,
+        saveUninitialized: true,
+        store: sessionStore,
+        cookie:
+        {
+                maxAge: 1000 * 60 * 60
+        },
+        rolling: true,
+}));
+
 router.get('/', async function(req, res, next) {
-  var user=await fetch('/user');
-  res.render('index', { user:user });
+  res.render('index', {user:req.session.user});
 });
 module.exports = router;
