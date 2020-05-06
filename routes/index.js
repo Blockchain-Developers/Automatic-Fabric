64e75767ad0a381e1f1b69b34b65be544f9dbb45
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-05-06 14:11:44 +0800

    session location change

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
-const mysql = require('mysql');
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'nodejs',
-  password: 'nodejspassword',
-  database: 'users',
-  multipleStatements: true
-});
-
-var sessionStore = new MySQLStore({}, con);
 
 /* GET home page. */
-router.use(session(
-{
-        secret: 'asdfasdI0YOYiAxjOtR',
-        resave: true,
-        saveUninitialized: true,
-        store: sessionStore,
-        cookie:
-        {
-                maxAge: 1000 * 60 * 60
-        },
-        rolling: true,
-}));
 
 router.get('/', async function(req, res, next) {
-  res.render('index', {user:req.session.user});
+  res.render('index', {user:req.session.user, isadmin:req.session.admin});
 });
 module.exports = router;
