6715cca47a5d3ca48935efac42ab5bd230e7aec7
root <root@ip-172-31-94-82.ec2.internal>
2020-05-13 09:54:16 +0000

    db connection resolved

--- routes/new.js
+++ routes/new.js
@@ -17,18 +17,52 @@ router.get('/', async function(req, res, next) {
   res.render('new');
 });
 router.post('/', async function(req, res, next){
+  var str='';
+  for(var i=0;i<req.body.fields.length;i++){
+    if(i!=0){
+      str+=', ';
+    }
+    str+="'";
+    str+=req.body.fields[i];
+    str+="'";
+  }
+  con.query("select username from users where role=? and username in ("+str+")", ['user'], async function(err, results){
+  if(results.length==req.body.fields.length){
   var data={};
   data.orgcount=req.body.fields.length;
   data.org=[];
-  for(var i=0;i<data.orgcount;i++){
-    data.org.push({});
-    data.org[i].name=req.body.fields[i];
-  }
   var id=await randomstring.generate({
     length: 10,
     charset: 'numeric'
   });
-  data_str=await JSON.stringify(data);
+
+  for(var i=0;i<data.orgcount;i++){
+    data.org.push({});
+    data.org[i].name=req.body.fields[i];
+    con.query('select username, data from users where username=?', req.body.fields[i], async function(err, results){
+      var userdata=await JSON.parse(results[0].data);
+      if(!userdata){
+        userdata={};
+      }
+      if(!userdata.pending){
+        userdata.pending=[];
+        userdata.pending.push(id);
+      }
+      else{
+        userdata.pending.push(id);
+      }
+      userdata_str=await JSON.stringify(userdata);
+      con.query('update users set data=? where username=?', [userdata_str, results[0].username]);
+    });
+  }
+  var data_str=await JSON.stringify(data);
   con.query('insert into pending set id=?, data=?', [id, data_str])
+  res.send('success')
+  }
+  else
+  {
+    res.send('non-existence member detected')
+  }
+  });
 });
 module.exports = router;
