61280240b827710405d9e3bd5c74351283074f6a
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-05-13 15:21:42 +0800

    add mysql

--- routes/new.js
+++ routes/new.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'nodejs',
+  password: 'nodejspassword',
+  database: 'users',
+  multipleStatements: true
+});
+
 /* GET home page. */
 
 router.get('/', async function(req, res, next) {
   res.render('', {user:req.session.user, isadmin:req.session.admin});
 });
 router.post('/', async function(req, res, next){
-  console.log(req.body)
+
 });
 module.exports = router;
