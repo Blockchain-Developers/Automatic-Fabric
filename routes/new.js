e4160f92179606478fbb0824091761579d5e58c2
root <root@ip-172-31-94-82.ec2.internal>
2020-05-13 07:44:01 +0000

    connected mysql

--- routes/new.js
+++ routes/new.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var randomstring = require("randomstring");
 
 const mysql = require('mysql');
 const con = mysql.createConnection({
@@ -16,13 +17,18 @@ router.get('/', async function(req, res, next) {
   res.render('new');
 });
 router.post('/', async function(req, res, next){
-  var data;
+  var data={};
   data.orgcount=req.body.fields.length;
   data.org=[];
   for(var i=0;i<data.orgcount;i++){
     data.org.push({});
     data.org[i].name=req.body.fields[i];
   }
-  
+  var id=await randomstring.generate({
+    length: 10,
+    charset: 'numeric'
+  });
+  data_str=await JSON.stringify(data);
+  con.query('insert into pending set id=?, data=?', [id, data_str])
 });
 module.exports = router;
