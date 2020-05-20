62c035e0fa0fa70a9bf42e9d775cfdff9cf20fb8
root <root@ip-172-31-94-82.ec2.internal>
2020-05-20 07:55:58 +0000

    filljs finish

--- routes/fill.js
+++ routes/fill.js
@@ -15,7 +15,7 @@ const con = mysql.createConnection({
 
 router.get('/:id', async function(req, res, next) {
   con.query('select data from pending where id=?', req.params.id, async function(err, results){
-    var data=await JSON.parse(results.[0].data);
+    var data=await JSON.parse(results[0].data);
     var decision=0;
     for(var i=0;i<data.orgcount;i++){
       if(data.org[i].name==req.session.user){
@@ -33,6 +33,40 @@ router.get('/:id', async function(req, res, next) {
 
 });
 router.post('/:id', async function(req, res, next){
-  console.log(req.body);
+  con.query('select data from pending where id=?', req.params.id, async function(err, results){
+    var data=await JSON.parse(results[0].data);
+    var decision=0;
+    var orgnum;
+    for(var i=0;i<data.orgcount;i++){
+      if(data.org[i].name==req.session.user){
+        decision=1;
+	orgnum=i;
+      }
+    }
+    if(decision){
+      data.org[orgnum].peercount=req.body.fields.length/2;
+      data.org[orgnum].orderer={port:req.body.ordererport};
+      data.org[orgnum].ca={port:req.body.caport};
+      data.org[orgnum].peer=[];
+      for(var i=0;i<data.org[orgnum].peercount;i++){
+        data.org[orgnum].peer.push({name:req.body.fields[2*i],port:req.body.fields[2*i+1]});
+      }
+      con.query('update pending set data=? where id=?', [await JSON.stringify(data), req.params.id]);
+      con.query('select data from users where username=?', req.session.user, async function(err, results){
+        var data=await JSON.parse(results[0].data);
+        for(var i=0;i<data.pending.length;i++){
+          if(data.pending[i].id==req.params.id){
+            data.pending[i].filled=1;
+          }
+        }
+      });
+      res.redirect('/')
+    }
+    else{
+      res.send('Illegal Request')
+    }
+
+  })
+
 });
 module.exports = router;
