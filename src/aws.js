06f8a2d4bd7ebeebbd66da284156bc1b36de9f83
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-07-06 14:59:36 +0800

    delete comments

--- src/aws.js
+++ src/aws.js
@@ -1,51 +1,6 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({ region: "us-east-1" });
-/*
-var instanceParams = {
-    LaunchTemplate: { LaunchTemplateId: "lt-0f9ee5f9a42217a4f" },
-    MaxCount: 1,
-    MinCount: 1,
-};
-
-const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" })
-    .runInstances(instanceParams)
-    .promise();
-
-ec2.then((data) => {
-    //console.log(data);
-    let instanceId = data.Instances[0].InstanceId;
-    //console.log("Created instance", instanceId);
-    tagParams = {
-        Resources: [instanceId],
-        Tags: [
-            {
-                Key: "Name",
-                Value: "SDK_GENERATE",
-            },
-        ],
-    };
-    // Create a promise on an EC2 service object
-    var tagPromise = new AWS.EC2({ apiVersion: "2016-11-15" })
-        .createTags(tagParams)
-        .promise();
-    // Handle promise's fulfilled/rejected states
-    tagPromise
-        .then(function (data) {
-            //console.log("Instance tagged");
-        })
-        .catch(function (err) {
-            console.error(err, err.stack);
-        });
-}).catch((err) => {
-    console.error(err, err.stack);
-});
-var params = {
-    InstanceIds:["i-0b1fcb6ef48f7b90a"]
-}
-const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" }).terminateInstances(params).promise()
-ec2.then( (data)=>{console.log(data)}).catch( (err) => {console.error(err)});
-*/
 
 const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 async function createNetworkInterface(networkconfig) {
@@ -59,14 +14,6 @@ async function createNetworkInterface(networkconfig) {
 
 async function allocateAddress() {
     let data = await ec2.allocateAddress({ Domain: "vpc" }).promise();
-    //console.log(data);
-    /*
-    data = {
-         AllocationId: "eipalloc-64d5890a",
-         Domain: "vpc",
-         Ip: "203.0.113.0"
-    }
-    */
     return data;
 }
 async function releaseAddress(allocateid) {
@@ -80,12 +27,6 @@ async function associateAddress(allocateid, networkid) {
         NetworkInterfaceId: networkid,
     };
     let data = await ec2.associateAddress(params).promise();
-    //console.log(data);
-    /*
-    data = {
-        AssociationId: "eipassoc-2bebb745"
-    }
-    */
     return data;
 }
 async function disassociateAddress(associteid) {
@@ -96,7 +37,6 @@ async function deleteNetworkInterface(networkid) {
     let data = await ec2
         .deleteNetworkInterface({ NetworkInterfaceId: networkid })
         .promise();
-    //console.log(data);
 }
 
 async function launchInstance(params) {
@@ -127,15 +67,6 @@ async function setupNetwork(use_public_ip = false) {
         );
         return { networkid, PrivateIpAddress };
     }
-
-    // let InstanceId = await launchInstance(instanceParams);
-    // let attachparams = {
-    //     NetworkInterfaceId: networkid,
-    //     InstanceId: InstanceId,
-    //     DeviceIndex: 0,
-    // };
-    // attachInterface(attachparams);
-    // deleteNetworkInterface("eni-0108653ef100950f3");
 }
 async function launchInstanceOfNetwork(networkid, filename) {
     let instanceParams = {
