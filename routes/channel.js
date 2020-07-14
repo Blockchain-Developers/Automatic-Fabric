1081153a854db7078942eeaf495426492a5349c1
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-07-14 16:22:15 +0800

    added join channel decision

--- routes/channel.js
+++ routes/channel.js
@@ -1,8 +1,9 @@
+var createError = require('http-errors');
 var express = require("express");
 var router = express.Router();
 var randomstring = require("randomstring");
 const { promisify } = require("util");
-
+const utilities = require('../src/utilities')
 const mysql = require("mysql");
 const con = mysql.createConnection({
     host: "localhost",
@@ -50,7 +51,7 @@ router.post("/:id/new", async function (req, res, next) {
   var participants=[];
   for(var i=0;i<results.length;i++){
     participants.push(results[i].username);
-    data.push({participant:results[i].username, decision:1});
+    data.push({participant:results[i].username, decision:0});
   }
   var channelid = await randomstring.generate({
       length: 10,
@@ -82,5 +83,65 @@ router.post("/:id/new", async function (req, res, next) {
   data=JSON.stringify(data);
   con.query('insert into channels set id=?, network=?, data=?, status=?', [channelid, req.params.id, data, 'pending'])
   res.redirect('/network/'+req.params.id)
+});
+router.get("/:networkid/:what/:channelid", async function (req, res, next) {
+  let err, results = await queryAsync(
+          "select username from users where role=? and username =? and data like ?",
+          [req.session.user, "user", '%'+req.params.networkid+'%']
+      );
+  if(results.length){
+    if(req.params.what=='join'){
+      con.query('select data from channels where id=? and network=?', [req.params.channelid, req.params.networkid], function(err, results){
+        var data=JSON.parse(results[0].data);
+        var cnt=0;
+        var orgs=[];
+        for(var i=0;i<data.length;i++){
+          if(data[i].participant==req.session.user){
+            data[i].participant=1;
+          }
+          if(data[i].decision){
+            cnt++;
+          }
+          if(data[i].decision==1){
+            orgs.push(data[i].participant);
+          }
+        }
+        if(cnt==data.length){
+          utilities.startchannel(orgs, req.params.networkid, req.params.channelid)
+        }
+        data=JSON.stringify(data);
+        con.query('update channels set data=? where id=? and network=?', [data, req.params.channelid, req.params.networkid]);
+      });
+    }else if(req.params.what=='deny') {
+      con.query('select data from channels where id=? and network=?', [req.params.channelid, req.params.networkid], function(err, results){
+        var data=JSON.parse(results[0].data);
+        var cnt=0;
+        var orgs=[];
+        for(var i=0;i<data.length;i++){
+          if(data[i].participant==req.session.user){
+            data[i].participant=2;
+          }
+          if(data[i].decision){
+            cnt++;
+          }
+          if(data[i].decision==1){
+            orgs.push(data[i].participant);
+          }
+        }
+        if(cnt==data.length){
+          utilities.startchannel(orgs, req.params.networkid, req.params.channelid)
+        }
+        data=JSON.stringify(data);
+        con.query('update channels set data=? where id=? and network=?', [data, req.params.channelid, req.params.networkid]);
+      });
+    }else if(req.params.what=='details'){
+      
+    }
+    res.redirect('/network/'+req.params.networkid)
+  }
+  else{
+    createError(403);
+  }
+
 });
 module.exports = router;
